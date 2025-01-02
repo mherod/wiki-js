64df8dfc443a8b3a8636bcadf9f74facd100a7b4
Matthew Herod <matthew.herod@gmail.com>
2025-01-02 16:28:12 +0000

    chore: update dependencies and migrate to ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        // Node.js globals
+        'process': 'readonly',
+        'module': 'readonly',
+        'require': 'readonly',
+        '__dirname': 'readonly',
+        '__filename': 'readonly',
+        'console': 'readonly',
+        'Buffer': 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs['recommended'].rules,
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+]; 
